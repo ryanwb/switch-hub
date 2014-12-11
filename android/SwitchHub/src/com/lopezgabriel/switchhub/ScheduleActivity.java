9cdf9b6c434aa83e566d435dbfb4ef1813f5a1a8
Gabriel Lopez <lopezg@Gabriels-MacBook-Pro.local>
2014-12-10 18:43:10 -0800

    missing file

---
+++ android/SwitchHub/src/com/lopezgabriel/switchhub/ScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.lopezgabriel.switchhub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduleActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    
+	}
+
+}
